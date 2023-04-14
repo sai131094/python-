d5dcd664af060427d342b54948a4a06e507f7376
sai131094 <saiibitdar123@gmail.com>
2023-04-15 00:50:39 +0530

    python

---
+++ hello.py
@@ -0,0 +1,21 @@
+a= complex(8,2)
+
+a1 = 125
+print(a)
+b = "sainath"
+print(a+a1)
+print("the type of a is", type(a))
+print("the type of b is",type(b))
+c = "madhura"
+print(type(c))
+
+list1 =[8, 2.3, [4,5],["apple","banana"]]
+
+print(list1)
+
+tuple1 = (("parrot","sparrow"),("lion","tiger"))
+
+print(tuple1)
+
+dict1 = {"name":"sainath", "age":27, "canvote":True}
+print(dict1)
---
+++ typecasting.py
@@ -0,0 +1,9 @@
+string = "15"
+ 
+number = 7
+ 
+string_number = int(string)
+ 
+sum = number + string_number
+ 
+print(sum)
